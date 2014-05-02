1817fd4d1ced7eedd4e87f3a6511e9c329a85657
Alexey Vakulich <soulSpringg@gmail.com>
2014-05-02 11:41:39 +0300

    added references on tables
    added references on tables

--- db-scheme/tables.sql
+++ db-scheme/tables.sql
@@ -2,9 +2,9 @@ drop table Branch;
 
 create table Branch
 (branchno char(5) not null,
- street varchar(35),
- city varchar(10),
- postcode varchar(10),
+ street varchar(35) not null,
+ city varchar(10) not null,
+ postcode varchar(10) not null,
  primary key(branchno)
 );
 
@@ -12,41 +12,45 @@ drop table Staff;
 
 create table Staff
 (staffno char(5) not null,
- fname varchar(10),
- lname varchar(10),
- position varchar(10),
+ fname varchar(10) not null,
+ lname varchar(10) not null,
+ position varchar(10) not null,
  sex char(1),
- dob char(9),
- salary smallint,
- branchno char(5),
- primary key(staffno)
+ dob date,
+ salary smallint not null,
+ branchno char(5) not null,
+ primary key(staffno),
+ foreign key(branchno) references Branch(branchno)
 );
 
 drop table PropertyForRent;
 
 create table PropertyForRent 
 (propertyno char(5) not null, 
- street varchar(35), 
- city varchar(10),
- postcode varchar(10),
- type varchar(10),
- rooms smallint,
- rent int,
- ownerno char(5),
+ street varchar(35) not null, 
+ city varchar(10) not null,
+ postcode varchar(10) not null,
+ type varchar(10) not null,
+ rooms smallint not null,
+ rent int not null,
+ ownerno char(5) not null,
  staffno char(5),
- branchno char(5),
- primary key(propertyno)
+ branchno char(5) not null,
+ primary key(propertyno),
+ foreign key(ownerno) references PrivateOwner(ownerno),
+ foreign key(staffno) references Staff(staffno),
+ foreign key(branchno) references Branch(branchno)
 );
 
 drop table Client;
 
 create table Client
 (clientno char(5) not null,
- fname varchar(10),
- lname varchar(10),
- telno char(15),
- preftype varchar(10),
- maxrent int,
+ fname varchar(10) not null,
+ lname varchar(10) not null,
+ telno char(15) not null,
+ preftype varchar(10) not null,
+ maxrent int not null,
  primary key(clientno)
 );
 
@@ -55,10 +59,10 @@ drop table PrivateOwner;
 
 create table PrivateOwner 
 (ownerno char(5) not null,
- fname varchar(10),
- lname varchar(10),
- address varchar(50),
- telno char(15),
+ fname varchar(10) not null,
+ lname varchar(10) not null,
+ address varchar(50) not null,
+ telno char(15) not null,
  primary key(ownerno)
 );
 
@@ -67,9 +71,11 @@ drop table Viewing;
 create table Viewing
 (clientno char(5) not null,
  propertyno char(5) not null,
- viewdate char(9),
- commnt varchar(15),
- primary key(clientno,propertyno)
+ viewdate date not null,
+ comments varchar(50),
+ primary key(clientno,propertyno),
+ foreign key(propertyno) references PropertyForRent(propertyno),
+ foreign key(clientno) references Client(clientno)
 );
 
 drop table Registration;
@@ -77,7 +83,10 @@ drop table Registration;
 create table Registration
 (clientNo char(5) not null,
 branchNo char(5) not null,
-staffNo char(5),
-dateJoined char(9),
-primary key(clientNo,branchNo)
+staffNo char(5) not null,
+dateJoined date not null,
+primary key(clientNo,branchNo),
+foreign key(clientNo) references Client(clientno),
+foreign key(branchNo) references Branch(branchno),
+foreign key(staffNo) references Staff(staffno)
 );
